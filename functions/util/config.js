ca88559fd022e93fd687c8d6dc4f67f3c938db25
Raimon <ramonkoudsi@gmail.com>
2020-06-13 21:03:23 -0930

    Initial commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDVz4vT1C_6Y9RkOrxM23Xv3xdLV7yjrNI",
+  authDomain: "socialbd-4fab1.firebaseapp.com",
+  databaseURL: "https://socialbd-4fab1.firebaseio.com",
+  projectId: "socialbd-4fab1",
+  storageBucket: "socialbd-4fab1.appspot.com",
+  messagingSenderId: "34162263862",
+  appId: "1:34162263862:web:5fe8c7f917332f4a329597",
+  measurementId: "G-YECY7VYBBB"
+};
